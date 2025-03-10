877609c2cc92ed6adf0fe73dcf64ef1457ec463c
Ukbe Akdogan <ukbe.akdogan@secureframe.com>
2025-03-10 04:01:53 +0300

    Update function reference

--- amplify/functions/post-confirmation/handler.ts
+++ amplify/functions/post-confirmation/handler.ts
@@ -3,7 +3,7 @@ import { type Schema } from "../../data/resource";
 import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
 import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
-import { env } from "$amplify/env/postConfirmation";
+import { env } from "$amplify/env/post-confirmation";
 
 const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(
   env
@@ -17,7 +17,7 @@ export const handler: PostConfirmationTriggerHandler = async (event) => {
   await client.models.User.create({
       email: event.request.userAttributes.email,
       totalCredits: 0,
-      id: event.request.userAttributes.sub,
+      id: event.request.userAttributes.userId,
       name: event.request.userAttributes.name
   });
 
